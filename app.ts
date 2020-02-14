8adc7a52b30a419ab2a27a7a9588947455741d2c
Wutang445 <austinw556@gmail.com>
2020-02-14 12:12:06 -0500

    object types

--- app.ts
+++ app.ts
@@ -1,1 +1,11 @@
-console.log('Your code goes here...');
+function add(num1: number, num2: number, showResult: boolean, phrase: string) {
+  return num1 + num2;
+}
+
+const n1 = 20;
+const n2 = 15;
+const printResult = 8;
+const aWord = "dog";
+
+const sum = add(n1, n2, printResult, aWord);
+console.log(sum);
---
+++ objects.ts
@@ -0,0 +1,9 @@
+const person: {
+  name: string;
+  age: number;
+} = {
+  name: "Donald",
+  age: 49
+};
+
+console.log(person.age);
